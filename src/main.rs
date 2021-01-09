f9e891c7cc19deab2265e5c2ed32049613b37623
Matt Pruitt <matt@guitsaru.com>
2021-01-09 19:08:01 +0700

    New command

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate exitcode;
 mod dragonruby;
 mod install;
+mod new;
 use clap::clap_app;
 
 fn main() {
@@ -8,15 +9,22 @@ fn main() {
         (version: "0.1.0")
         (author: "Matt Pruitt <matt@guitsaru.com>")
         (about: "Installs DragonRuby dependencies")
+        (setting: clap::AppSettings::ArgRequiredElseHelp)
         (@arg verbose: -v --verbose "Displays more information")
         (@subcommand install =>
             (about: "Installs DragonRuby")
             (@arg FILE: +required "The location of the DragonRuby Game Toolkit zip file")
         )
+        (@subcommand new =>
+            (about: "Start a new DragonRuby project")
+            (@arg PATH: +required "The path to your new project")
+        )
     )
     .get_matches();
 
-    if let Some(ref matches) = matches.subcommand_matches("install") {
-        install::install(matches);
+    match matches.subcommand_name() {
+        Some("install") => install::install(&matches.subcommand_matches("install").unwrap()),
+        Some("new") => new::new(&matches.subcommand_matches("new").unwrap()),
+        _ => unreachable!(),
     }
 }
---
+++ src/new.rs
@@ -0,0 +1,37 @@
+use crate::dragonruby;
+use std::fs;
+use std::path::Path;
+use std::path::PathBuf;
+use std::process;
+use walkdir::WalkDir;
+
+pub fn new(matches: &&clap::ArgMatches) {
+  dragonruby::ensure_installed();
+
+  let path = matches.value_of("PATH").unwrap();
+  let destination = Path::new(path);
+
+  if destination.exists() {
+    println!("{} already exists", path);
+    process::exit(exitcode::DATAERR);
+  }
+
+  fs::create_dir(destination).unwrap();
+
+  let template = dragonruby::dragonruby_directory().join("mygame");
+  copy_directory(template, destination.to_path_buf());
+}
+
+fn copy_directory(source: PathBuf, destination: PathBuf) {
+  for entry in WalkDir::new(source.clone()) {
+    let entry = entry.unwrap();
+    let base = entry.path().strip_prefix(source.clone()).unwrap();
+    let file_source = source.clone().join(base);
+    let file_destination = destination.clone().join(base);
+
+    if file_source.is_file() {
+      fs::create_dir_all(file_destination.parent().unwrap()).unwrap();
+      fs::copy(file_source, file_destination).unwrap();
+    }
+  }
+}
