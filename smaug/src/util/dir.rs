44190ab2d7c7f686c34a6585948f4dc2cbf64b88
Matt Pruitt <matt.pruitt@blockfi.com>
2021-04-24 13:29:03 +0700

    Respect `.smaugignore` file (#41)

--- smaug/src/util/dir.rs
+++ smaug/src/util/dir.rs
@@ -1,3 +1,4 @@
+use ignore::gitignore::{Gitignore, GitignoreBuilder};
 use log::*;
 use std::fs;
 use std::io;
@@ -5,13 +6,24 @@ use std::path::Path;
 use walkdir::WalkDir;
 
 pub fn copy_directory<P: AsRef<Path>>(source: &P, destination: &P) -> io::Result<()> {
+    let mut ignore_builder = GitignoreBuilder::new(source);
+    let ignore_file = source.as_ref().join(".smaugignore");
+
+    if ignore_file.is_file() {
+        ignore_builder.add(ignore_file);
+    }
+
+    let ignore = ignore_builder
+        .build()
+        .expect("Could not parse smaugignore file");
+
     for entry in WalkDir::new(source) {
         let entry = entry.expect("Could not find directory");
         let entry = entry.path();
         let relative = entry.strip_prefix(source.as_ref()).unwrap();
         let new_path = destination.as_ref().join(relative);
 
-        if entry.is_file() && !is_git_dir(entry.to_str().unwrap()) {
+        if entry.is_file() && !is_ignored(entry, &ignore) {
             trace!(
                 "Creating directory {}",
                 new_path.parent().and_then(|p| p.to_str()).unwrap()
@@ -31,4 +43,20 @@ pub fn copy_directory<P: AsRef<Path>>(source: &P, destination: &P) -> io::Result
 
 fn is_git_dir(path: &str) -> bool {
     path.contains("/.git/") || path.contains("\\.git\\")
+}
+
+fn is_ignored(path: &Path, ignore: &Gitignore) -> bool {
+    if matches_ignore(path, ignore) {
+        trace!("Ignoring {:?}", path);
+        return true;
+    }
+
+    false
+}
+
+fn matches_ignore(path: &Path, ignore: &Gitignore) -> bool {
+    ignore
+        .matched_path_or_any_parents(path, path.is_dir())
+        .is_ignore()
+        || is_git_dir(path.to_string_lossy().as_ref())
 }
